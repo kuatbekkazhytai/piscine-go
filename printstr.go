3b11c6e2ee8b452884d5dfab45999bb8c6b696e0
kuatbekkazhytai <kuatbek.kazhytai@nu.edu.kz>
2019-10-17 20:42:14 +0600

    asdasda

--- printstr.go
+++ printstr.go
@@ -4,8 +4,8 @@ import "github.com/01-edu/z01"
 
 func PrintStr(str string) {
 
-stringchange := []rune(str)
-			for v := range stringchange{
-			z01.Println(stringchange[v])
-            }
+	stringchange := []rune(str)
+	for v := range stringchange {
+		z01.Println(stringchange[v])
+	}
 }
