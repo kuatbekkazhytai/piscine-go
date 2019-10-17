140437f2f1cc77ee2178c1dcf4e9550504bc9b1c
kuatbekkazhytai <kuatbek.kazhytai@nu.edu.kz>
2019-10-17 20:29:10 +0600

    origin

--- printstr.go
+++ printstr.go
@@ -2,17 +2,10 @@ package main
 
 import "github.com/01-edu/z01"
 
-func main() {
-	str := "Hello World!"
-	PrintStr(str)
-}
-
-
 func PrintStr(str string) {
 
 stringchange := []rune(str)
-for v := range stringchange{
-z01.Println(stringchange[v])
-
-}
+			for v := range stringchange{
+			z01.Println(stringchange[v])
+            }
 }
