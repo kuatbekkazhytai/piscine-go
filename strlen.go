0504ee6c4362b6550057611e2520d7d2b3a5eee4
kuatbekkazhytai <kuatbek.kazhytai@nu.edu.kz>
2019-10-17 21:54:03 +0600

    strlen

--- strlen.go
+++ strlen.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func StrLen(str string) int {
-		change := []rune(str)
-		var countlen int = 0
-		for i := range change {
-			countlen++
-			i = i
-		}
-return countlen
+	change := []rune(str)
+	var countlen int = 0
+	for i := range change {
+		countlen++
+		i = i
+	}
+	return countlen
 }
