469e8711dcdb89be4548cf031415f1026474c709
kuatbekkazhytai <kuatbek.kazhytai@nu.edu.kz>
2019-10-21 12:26:28 +0600

    iterative

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,10 +1,13 @@
-package piscine
+package main
 
 func IterativeFactorial(nb int) int {
-	result := 1
-	for i := 1; i <= nb; i++ {
-		result = result * i
+	if nb > 0 {
+		result := 1
+		for i := 1; i <= nb; i++ {
+			result = result * i
+		}
+		return result
+	} else {
+		return 0
 	}
-
-	return result
 }
