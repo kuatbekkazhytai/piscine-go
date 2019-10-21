03b5c167d5e73ef2fcde5aabab695e1644f5a267
kuatbekkazhytai <kuatbek.kazhytai@nu.edu.kz>
2019-10-21 16:02:22 +0600

    fibbo

---
+++ fibonacci.go
@@ -0,0 +1,16 @@
+package piscine
+
+func Fibonacci(index int) int {
+
+	if index < 0 {
+		return -1
+	} else if index > 1 {
+		return Fibonacci(index-1) + Fibonacci(index-2)
+
+	} else if index == 1 {
+		return 1
+	} else {
+		return 0
+	}
+
+}
