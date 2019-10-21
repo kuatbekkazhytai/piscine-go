9f93a6a0d1846bc43168e70298192095845d451f
kuatbekkazhytai <kuatbek.kazhytai@nu.edu.kz>
2019-10-21 13:00:12 +0600

    recursive

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,18 +1,11 @@
-package main
-
-import "fmt"
+package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb == 1 || nb == 0 {
+	if nb ==1 || nb ==0 {
 		return 1
-	} else if nb > 1 && nb < 20 {
-		return nb * RecursiveFactorial(nb-1)
+	} else if nb > 1 &&  nb < 20{
+		return nb*RecursiveFactorial(nb-1)
 	} else {
 		return 0
 	}
 
-}
-func main() {
-	arg := 4
-	fmt.Println(RecursiveFactorial(arg))
-}
