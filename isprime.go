42bdcfdcd1f9e1febabb9d6822c5dec92b3183e0
kuatbekkazhytai <kuatbek.kazhytai@nu.edu.kz>
2019-10-21 18:29:22 +0600

    isprime

--- isprime.go
+++ isprime.go
@@ -1,26 +1,20 @@
-package main
+package piscine
 
-import "fmt"
-
-func IsPrime(nb int) bool {
+func IsPrime(nb int) bool { 
 	if nb <= 1 {
 		return false
-	} else if nb == 2 {
+	} else if nb == {
 		return true
 	} else {
 
-		if nb%2 == 0 {
-			return false
-		}
-		for i := 3; i*i <= nb; i += 2 {
-			if nb%i == 0 {
-				return false
+		if nb % 2 == {
+			return false 
+			
+		for i := 3; i*i <= nb; i += 2{
+				if nb%i == 0{
+					return false
+				}
 			}
-		}
-		return true
+	return true
 	}
-}
-func main() {
-	fmt.Println(IsPrime(5))
-	fmt.Println(IsPrime(4))
 }
