cf5b838363dd9e9c40625e3ba675478b08525f1f
kuatbekkazhytai <kuatbek.kazhytai@nu.edu.kz>
2019-10-21 11:44:47 +0600

    raid1a

--- raid1a.go
+++ raid1a.go
@@ -1,24 +1,29 @@
-package main
+package student
 
 import "github.com/01-edu/z01"
 
-func Raid1a(x,y int) {
-	for i := 1; i <= y; i++ {
-		if i == y || i == 1 {
-			for j := 1; j <= x; j++ {
-				if j == x || j == 1 {
-					z01.PrintRune('o')
-				} else {
-					z01.PrintRune('-')
+func main() {
+	Raid1a(-1, 6)
+}
+
+func Raid1a(x, y int) {
+	if x > 0 && y > 0 {
+		for i := 1; i <= y; i++ {
+			if i == y || i == 1 {
+				for j := 1; j <= x; j++ {
+					if j == x || j == 1 {
+						z01.PrintRune('o')
+					} else {
+						z01.PrintRune('-')
+					}
 				}
-			}
-			z01.PrintRune('\n')
-		} else {
-			for j := 1; j <= x; j++ {
-				if j == x || j == 1 {
-					z01.PrintRune('|')
-				} else {
-					z01.PrintRune(' ')
+			} else {
+				for j := 1; j <= x; j++ {
+					if j == x || j == 1 {
+						z01.PrintRune('|')
+					} else {
+						z01.PrintRune(' ')
+					}
 				}
 			}
 			z01.PrintRune('\n')
