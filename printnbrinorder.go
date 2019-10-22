295d660b52f7f4d86e58a5e31aec027abfdc5e9e
kuatbekkazhytai <kuatbek.kazhytai@nu.edu.kz>
2019-10-22 13:05:21 +0600

    printnbrinorder

---
+++ printnbrinorder.go
@@ -0,0 +1,23 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbrInOrder(n int) {
+
+        var x []int
+        if n == 0 {
+                x = append(x, 0)
+        }
+
+        for i := 0; n > 0; i++ {
+                x = append(x, n%10)
+                n = n / 10
+
+        }
+        SortIntegerTable(t)
+        for i := range x {
+                z01.PrintRune(rune('0' + x[i]))
+        }
+}
+
+
