379e5785a16637c6c6873a08a38d2b895006f6d1
kuatbekkazhytai <kuatbek.kazhytai@nu.edu.kz>
2019-10-22 19:41:15 +0600

    lastrune

--- lastrune.go
+++ lastrune.go
@@ -1,22 +1,5 @@
 package piscine
 
-func NRune(s string, n int) rune {
-
-	change := []rune(s)
-	var count int = 0
-
-	for i := range change {
-		count++
-		i = i
-	}
-	if n <= count && count != 0 && n >= 0 {
-
-		return change[n-1]
-
-	} else {
-		return 0
-	}
-}
 func LastRune(s string) rune {
 
 	change := []rune(s)
