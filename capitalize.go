8a071cdc9bff44a28515a75de6a65ca7370210e3
kuatbekkazhytai <kuatbek.kazhytai@nu.edu.kz>
2019-10-23 00:48:55 +0600

    capital

--- capitalize.go
+++ capitalize.go
@@ -3,15 +3,15 @@ package piscine
 func Capitalize(s string) string {
 	change := []rune(s)
 	FirstLetter := true
-	for i := range runes {
+	for i := range change {
 		if change[i] >= 'a' && change[i] <= 'z' && FirstLetter {
 			change[i] -= 32
 			FirstLetter = false
 		} else if (IsRuneDigit(change[i]) || (change[i] >= 'A' && change[i] <= 'Z')) && FirstLetter {
 			FirstLetter = false
-		} else if !isFirstLetter && change[i] >= 'A' && change[i] <= 'Z' {
+		} else if !FirstLetter && change[i] >= 'A' && change[i] <= 'Z' {
 			change[i] += 32
-		} else if !isFirstLetter && !IsRuneDigit(change[i]) && !IsRuneAlpha(change[i]) {
+		} else if !FirstLetter && !IsRuneDigit(change[i]) && !IsRuneAlpha(change[i]) {
 			FirstLetter = true
 		}
 	}
