f0dc7c3a7e808b333e98ee9ed6bb6756464e9261
kuatbekkazhytai <kuatbek.kazhytai@nu.edu.kz>
2019-10-22 19:31:10 +0600

    nrune

--- nrune.go
+++ nrune.go
@@ -1,6 +1,4 @@
-package main
-
-import "github.com/01-edu/z01"
+package piscine
 
 func NRune(s string, n int) rune {
 
@@ -8,20 +6,14 @@ func NRune(s string, n int) rune {
 	var count int = 0
 
 	for i := range change {
-		count++
+		count ++
 		i = i
 	}
-	if n <= count && count != 0 && n >= 0 {
-
+	if n <= count && count !=0 && n>=0 {
+	
 		return change[n-1]
 
 	} else {
 		return 0
 	}
-}
-func main() {
-	z01.PrintRune(NRune("Hello!", 3))
-	z01.PrintRune(NRune("Salut!", 2))
-	z01.PrintRune(NRune("Ola!", 4))
-	z01.PrintRune('\n')
 }
