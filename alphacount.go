7f74bbd1f0cf3d8bf541e99bde7969881852ecb1
kuatbekkazhytai <kuatbek.kazhytai@nu.edu.kz>
2019-10-22 10:47:42 +0600

    alphacout

--- alphacount.go
+++ alphacount.go
@@ -5,7 +5,7 @@ func AlphaCount(str string) int {
 	count := 0
 	stringch := []rune(str)
 	for index, word := range stringch {
-		if word > 65 && word < 90 || word > 97 && word < 122 {
+		if word >= 65 && word <= 90 || word >= 97 && word <= 122 {
 			count++
 			index = index
 		}
