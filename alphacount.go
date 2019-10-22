9e6e62e31271dc2c69214479c13d937cce3cc6a6
kuatbekkazhytai <kuatbek.kazhytai@nu.edu.kz>
2019-10-22 10:41:01 +0600

    alphacout

---
+++ alphacount.go
@@ -0,0 +1,14 @@
+package main
+
+func AlphaCount(str string) int {
+
+	count := 0
+	stringch := []rune(str)
+	for index, word := range stringch {
+		if word > 65 && word < 90 || word > 97 && word < 122 {
+			count++
+			index = index
+		}
+	}
+	return count
+}
