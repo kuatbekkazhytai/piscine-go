fef12b8e49318bfcb20191ddf8486cda3db2d707
kuatbekkazhytai <kuatbek.kazhytai@nu.edu.kz>
2019-10-22 13:06:17 +0600

    printnbrinorder

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -4,20 +4,18 @@ import "github.com/01-edu/z01"
 
 func PrintNbrInOrder(n int) {
 
-        var x []int
-        if n == 0 {
-                x = append(x, 0)
-        }
+	var x []int
+	if n == 0 {
+		x = append(x, 0)
+	}
 
-        for i := 0; n > 0; i++ {
-                x = append(x, n%10)
-                n = n / 10
+	for i := 0; n > 0; i++ {
+		x = append(x, n%10)
+		n = n / 10
 
-        }
-        SortIntegerTable(t)
-        for i := range x {
-                z01.PrintRune(rune('0' + x[i]))
-        }
+	}
+	SortIntegerTable(t)
+	for i := range x {
+		z01.PrintRune(rune('0' + x[i]))
+	}
 }
-
-
