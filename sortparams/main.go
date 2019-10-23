22b8ec74b9eae85147c179d3aecd6f920b50c5e2
kuatbekkazhytai <kuatbek.kazhytai@nu.edu.kz>
2019-10-23 23:22:14 +0600

    sort

---
+++ sortparams/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+	len := 0
+	t := ""
+	for i := range os.Args {
+		length = i
+	}
+	for i := 1; i <= len; i++ {
+		for j := i + 1; j <= len; j++ {
+			if Compare(os.Args[i], os.Args[j]) == 1 {
+				t = os.Args[i]
+				os.Args[i] = os.Args[j]
+				os.Args[j] = t
+			}
+		}
+		PrintStr(os.Args[i])
+		z01.PrintRune('\n')
+	}
+}
