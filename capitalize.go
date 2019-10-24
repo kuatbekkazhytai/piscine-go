ef56e758c353ca7638e1ce80801fe4888c37590b
kuatbekkazhytai <53311403+kuatbekkazhytai@users.noreply.github.com>
2019-10-24 20:20:38 +0600

    Update capitalize.go

--- capitalize.go
+++ capitalize.go
@@ -9,7 +9,7 @@ func Capitalize(s string) string {
 			isFirstLetter = false
 		} else if (change[i] >= '0' && change[i] <= '9') || (change[i] >= 65 && change[i] <= 90) && isFirstLetter {
 			isFirstLetter = false
-		} else if isFirstLetter && change[i] >= 65 && change[i] <= 90 {
+		} else if !isFirstLetter && change[i] >= 65 && change[i] <= 90 {
 			change[i] += 32
 		} else if !isFirstLetter && !(change[i] >= '0' && change[i] <= '9') && !(change[i] >= 97 && change[i] <= 122 || change[i] >= 65 && change[i] <= 90) {
 			isFirstLetter = true
